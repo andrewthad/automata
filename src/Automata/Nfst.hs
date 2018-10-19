c5c229b84f85c21bdab4a62d805c011cebe74e72
Andrew Martin <andrew.thaddeus@gmail.com>
2018-10-19 15:02:31 -0400

    add function for evaluating NFST on a bytestring

--- src/Automata/Nfst.hs
+++ src/Automata/Nfst.hs
@@ -13,6 +13,7 @@ module Automata.Nfst
     Nfst
     -- ** Functions
   , evaluate
+  , evaluateAscii
   , union
   , toDfst
   , toNfsa
@@ -33,15 +34,17 @@ module Automata.Nfst
 import Automata.Internal (State(..),Epsilon(..),Nfsa(..),Dfsa(..),TransitionNfsa(..),toDfsaMapping)
 import Automata.Internal.Transducer (Nfst(..),Dfst(..),TransitionNfst(..),MotionDfst(..),Edge(..),EdgeDest(..),epsilonClosure,rejection,union)
 import Control.Monad.ST (runST)
+import Data.ByteString (ByteString)
+import Data.Foldable (for_,fold)
+import Data.Map.Strict (Map)
 import Data.Maybe (fromMaybe)
 import Data.Monoid (Any(..))
-import Data.Set (Set)
-import Data.Map.Strict (Map)
 import Data.Primitive (Array,indexArray)
-import Data.Foldable (for_,fold)
+import Data.Set (Set)
 
 import Debug.Trace
 
+import qualified Data.ByteString.Char8 as BC
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import qualified Data.Set.Unboxed as SU
@@ -67,6 +70,19 @@ evaluate (Nfst transitions finals) tokens = S.unions $ M.elems $ M.filterWithKey
   (F.foldl' step (M.unionsWith (<>) (map (\s -> M.singleton s (S.singleton [])) (SU.toList (transitionNfstEpsilon (C.index transitions 0))))) tokens)
   where
   step :: Map Int (Set [m]) -> t -> Map Int (Set [m])
+  step active token = M.unionsWith (<>) $ M.foldlWithKey'
+    ( \xs state outputSets -> MLN.foldlWithKey'
+        (\zs outputTokenNext nextStates -> M.unionsWith (<>) (map (\s -> M.singleton s (S.mapMonotonic (outputTokenNext:) outputSets)) (SU.toList nextStates)) : zs)
+        xs
+        (DM.lookup token (transitionNfstConsume (C.index transitions state)))
+    ) [] active
+
+evaluateAscii :: forall m. Ord m => Nfst Char m -> ByteString -> Set [m]
+evaluateAscii (Nfst transitions finals) tokens = S.unions $ M.elems $ M.filterWithKey
+  (\k _ -> SU.member k finals)
+  (BC.foldl' step (M.unionsWith (<>) (map (\s -> M.singleton s (S.singleton [])) (SU.toList (transitionNfstEpsilon (C.index transitions 0))))) tokens)
+  where
+  step :: Map Int (Set [m]) -> Char -> Map Int (Set [m])
   step active token = M.unionsWith (<>) $ M.foldlWithKey'
     ( \xs state outputSets -> MLN.foldlWithKey'
         (\zs outputTokenNext nextStates -> M.unionsWith (<>) (map (\s -> M.singleton s (S.mapMonotonic (outputTokenNext:) outputSets)) (SU.toList nextStates)) : zs)
